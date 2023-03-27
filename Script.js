603e0afe69a4ff3897a09d80edd32c5f07bdf882
Muhammad Ahmed <119342876+MuhammadAhmed-Developer@users.noreply.github.com>
2023-03-27 18:41:48 +0100

    create weater Function

--- Script.js
+++ Script.js
@@ -22,24 +22,48 @@
 
 // 2. Write a js program to find maximum between three numbers.
 
-var num1 =+ prompt('Enter Number1')
-var num2 =+ prompt('Enter Number2')
-var num3 =+ prompt('Enter Number3')
-if(num2 > num1 && num2 > num3){
-   console.log(`your Number2 "${num2}" is greater then Number1 "${num1}" and Number3 "${num3}"`)
-}
-else if(num1 > num2 && num1 > num3){
-   console.log(`Your Number1 "${num1}" is greater then Number2 "${num2}" and Number3 "${num3}"`)
+// var num1 =+ prompt('Enter Number1')
+// var num2 =+ prompt('Enter Number2')
+// var num3 =+ prompt('Enter Number3')
+// if(num2 > num1 && num2 > num3){
+//    console.log(`your Number2 "${num2}" is greater then Number1 "${num1}" and Number3 "${num3}"`)
+// }
+// else if(num1 > num2 && num1 > num3){
+//    console.log(`Your Number1 "${num1}" is greater then Number2 "${num2}" and Number3 "${num3}"`)
    
-}
-else if(num3 > num1 && num3 > num2){
-   console.log(`Your Number3 "${num3}" is greater then Number1 "${num1}" and Number2 "${num2}"`)
+// }
+// else if(num3 > num1 && num3 > num2){
+//    console.log(`Your Number3 "${num3}" is greater then Number1 "${num1}" and Number2 "${num2}"`)
    
-}
-else if(num1 == num2 && num1 == num3 && num2 == num1 && num2 == num3 && num3 == num1 && num3 == num2){
-   console.log(`Your Numbers "${num1}" =  "${num2}" = "${num3}"`)
+// }
+// else if(num1 == num2 && num1 == num3 && num2 == num1 && num2 == num3 && num3 == num1 && num3 == num2){
+//    console.log(`Your Numbers "${num1}" =  "${num2}" = "${num3}"`)
    
+// }
+// else{
+//     console.log("Enter a valid value! Please Enter only Numbers")
+// }
+
+
+// 3. Write a js program to check whether a number is negative, positive or zero.
+
+
+var weather =+ prompt('Enter Weather Temperature in Numbers')
+if(weather <= 3){
+  console.log(`Your Temperature is  '${weather}⁰C' its means weather is very cold`)
+}
+ else if(weather <= 10){
+  console.log(`Your Temperature is  '${weather}⁰C' its means weather is cold`)
+}
+ else if(weather <= 25){
+  console.log(`Your Temperature is  '${weather}⁰C' its means weather is Normal`)
+}
+ else if(weather <= 40){
+  console.log(`Your Temperature is  '${weather}⁰C' its means weather is Hot`)
+}
+ else if(weather > 40){
+  console.log(`Your Temperature is  '${weather}⁰C' its means weather is very Hot`)
 }
 else{
-    console.log("Enter a valid value! Please Enter only Numbers")
+   console.log("Enter Valid Value! Please Enter Temp in Numbers")
 }
